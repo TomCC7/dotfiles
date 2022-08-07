6d41cb8b75a4097972717b9442cfef9316dd86c9
cc <cctom@umich.edu>
2022-08-07 10:15:22 -0400

    update

--- .config/polybar/music.sh
+++ .config/polybar/music.sh
@@ -4,10 +4,6 @@
 
 exec 2>/dev/null
 
-if [ "$(playerctl status)" = "Playing" ]; then
-  title=`exec playerctl metadata xesam:title`
-  artist=`exec playerctl metadata xesam:artist`
-  echo "[$artist] $title"
-else
-  echo "No song currently playing"
-fi
+title=`exec playerctl metadata xesam:title`
+artist=`exec playerctl metadata xesam:artist`
+echo "[$artist] $title"
