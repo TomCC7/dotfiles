ff22e7bc46511b1256dc46a72c5cafd1d2aa7689
cc <cctom@umich.edu>
2022-08-08 10:22:06 -0400

    update

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -125,7 +125,7 @@
 ;; }}
 
 ;; enable wakatime
-;; (global-wakatime-mode)
+(global-wakatime-mode)
 
 ;; add urdf and launch to xml-mode
 (use-package! nxml-mode
--- .config/doom/init.el
+++ .config/doom/init.el
@@ -73,7 +73,7 @@
        ;; vc                ; version-control and Emacs, sitting in a tree
 
        :term
-       ;;eshell            ; the elisp shell that works everywhere
+       eshell            ; the elisp shell that works everywhere
        ;;shell             ; simple shell REPL for Emacs
        ;;term              ; basic terminal emulator for Emacs
        vterm             ; the best terminal emulation in Emacs
@@ -96,7 +96,7 @@
        lsp
        magit             ; a git porcelain for Emacs
        make              ; run make tasks from Emacs
-       ;;pass              ; password manager for nerds
+       ;; pass              ; password manager for nerds
        pdf               ; pdf enhancements
        ;;prodigy           ; FIXME managing external services & code builders
        ;;rgb               ; creating color strings
